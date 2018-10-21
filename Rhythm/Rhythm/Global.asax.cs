f5cc6831da22c1b15c5eef723e66c0af4c7dd963
Maninder <rhythmmvc@gmail.com>
2018-10-21 22:47:45 +0530

    TestCommit-1

--- Rhythm/Rhythm/Global.asax.cs
+++ Rhythm/Rhythm/Global.asax.cs
@@ -12,6 +12,7 @@ namespace Rhythm
     {
         protected void Application_Start()
         {
+            //Test Commit
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
